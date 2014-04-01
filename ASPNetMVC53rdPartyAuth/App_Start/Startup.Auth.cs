004861e95c8047d8822dff2d3d1d09f869efb045
Maksood Mohiuddin <maksood@gmail.com>
2014-04-01 15:01:46 -0400

    removed sercret keys

--- ASPNetMVC53rdPartyAuth/App_Start/Startup.Auth.cs
+++ ASPNetMVC53rdPartyAuth/App_Start/Startup.Auth.cs
@@ -24,7 +24,7 @@ namespace ASPNetMVC53rdPartyAuth
             // You have to register this app at https://account.live.com/developers/applications and get the clientId and clientSecret
             app.UseMicrosoftAccountAuthentication(
                 clientId: "0000000044114C8C",
-                clientSecret: "6HMtvx2I5Y6OYEz6mh9dgUxiZVuUphlK");
+                clientSecret: "xxxxxxxxxxxxx");
 
             // You have to register this app at https://apps.twitter.com/ and get the consumerKey(API key) and consumerSecret(API secret).    
             // Twitter requires valid url format otherwise it won't work. But it can be placeholder values. 
@@ -32,13 +32,13 @@ namespace ASPNetMVC53rdPartyAuth
             // and Callback URL at http://xxx.com/twitter/callback format (i.e. http://www.example.com/twitter/callback)
             app.UseTwitterAuthentication(
                consumerKey: "HwlzBpNMgAjVOaPnDmYxCyOUW",
-               consumerSecret: "ZVeON9kydV6QzRZsesfH7CZVuewnBN9vsQ8aO0bUVf3cuppr5o");
+               consumerSecret: "xxxxxxxxxxxxxxxxx");
 
             // You have to register this app at https://developers.facebook.com/ and get the appId and appSecret.   
             // Facebook requires SSL, so that need to be enanbled.  Project url can be found under project properties and can be localhost.
             app.UseFacebookAuthentication(
                appId: "571724576260174",
-               appSecret: "38ace59395a3e7ed35b08f82b070bf45");
+               appSecret: "xxxxxxxxxxxxxxxx");
 
             // Google is easiest to implement, just need the following: 
             app.UseGoogleAuthentication();
