5d478dd172a6eaea9b583302ae118bd87215fb63
Maksood Mohiuddin <maksood@gmail.com>
2014-04-01 15:27:17 -0400

    Changed default routing to bring in login page at app start and changed app texts

--- ASPNetMVC53rdPartyAuth/App_Start/RouteConfig.cs
+++ ASPNetMVC53rdPartyAuth/App_Start/RouteConfig.cs
@@ -16,8 +16,14 @@ namespace ASPNetMVC53rdPartyAuth
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+               name: "Home",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
         }
     }
 }
--- ASPNetMVC53rdPartyAuth/Controllers/HomeController.cs
+++ ASPNetMVC53rdPartyAuth/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace ASPNetMVC53rdPartyAuth.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact page.";
 
             return View();
         }
