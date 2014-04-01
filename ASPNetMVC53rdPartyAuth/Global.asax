b5fe47e00aa02eac80427188c59659856d56ff32
Maksood Mohiuddin <maksood@gmail.com>
2014-04-01 14:36:57 -0400

    Checking into Github from Visual Studio 2013

---
+++ ASPNetMVC53rdPartyAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNetMVC53rdPartyAuth.MvcApplication" Language="C#" %>
---
+++ ASPNetMVC53rdPartyAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNetMVC53rdPartyAuth.Startup))]
+namespace ASPNetMVC53rdPartyAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
